ed4277ebbd0f1d55e86cf9a8f038e498e01af1df
kwon15477 <kwon15477@gmail.com>
2026-03-04 16:15:41 +0900

    /wiseSayings

---
+++ src/main/java/com/back/AppConfig.java
@@ -0,0 +1,65 @@
+package com.back;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.core.annotation.Order;
+
+@Configuration
+public class AppConfig {
+
+//    @Bean
+//    public PersonService personServiceV2() {
+//        System.out.println("personService v2 빈이 생성되었습니다.");
+//        return new PersonService(2);
+//    }
+//
+//    @Bean
+//    public PersonService personServiceV3() {
+//        System.out.println("personService v3 빈이 생성되었습니다.");
+//        return new PersonService(3);
+//    }
+
+    @Bean
+    public int personVersion() {
+        return 100;
+    }
+
+    @Bean
+    @Order(1)
+    public ApplicationRunner myApplicationRunner1() {
+        return args -> {
+            System.out.println("MyApplicationRunner1 is running");
+        };
+    }
+
+    @Bean
+    @Order(2)
+    public ApplicationRunner myApplicationRunner2() {
+        return args -> {
+            System.out.println("MyApplicationRunner2 is running");
+        };
+    }
+
+    @Autowired
+    @Lazy
+    private AppConfig self;
+
+    @Bean
+    public ApplicationRunner myApplicationRunner3() {
+        return args -> {
+            self.work1();
+            self.work2();
+        };
+    }
+
+    private void work1() {
+        System.out.println("work 1 is running");
+    }
+
+    private void work2() {
+        System.out.println("work 2 is running");
+    }
+}
---
+++ src/main/java/com/back/PersonController.java
@@ -0,0 +1,20 @@
+package com.back;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class PersonController {
+
+    private final PersonService personService;
+
+    @GetMapping("/people")
+    @ResponseBody
+    public String people() {
+        System.out.println("tesr");
+        return "사람 수 : %d".formatted(personService.count());
+    }
+}
